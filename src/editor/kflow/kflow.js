744c50405c8ce9f2a30dcf4e00227ac5a5f286c0
Tomoya Koyanagi <tomk79@gmail.com>
2025-01-23 02:28:33 +0900

    Kaleflowerコンテンツの編集機能を追加した。(5)

--- src/editor/kflow/kflow.js
+++ src/editor/kflow/kflow.js
@@ -4,6 +4,7 @@
 module.exports = function(px2ce){
 	var _this = this;
 	var $ = require('jquery');
+	var it79 = require('iterate79');
 	var px2style = px2ce.px2style;
 	var $canvas = $(px2ce.getElmCanvas());
 	var Promise = require('es6-promise').Promise;
@@ -33,7 +34,9 @@ module.exports = function(px2ce){
 					"btns": btns,
 					"onFinish": function(){
 						// 完了イベント
-						px2ce.finish();
+						saveContentsSrc(() => {
+							px2ce.finish();
+						});
 					}
 				},function(){
 					rlv();
@@ -74,4 +77,50 @@ module.exports = function(px2ce){
 
 	};
 
+	/**
+	 * 編集したコンテンツを保存する
+	 */
+	function saveContentsSrc(callback){
+		let data = kaleflower.get();
+		console.info(data);
+
+		const codes = {
+			'html': kaleflower.get(),
+			'css': '',
+			'js': '',
+		};
+
+		px2ce.gpiBridge(
+			{
+				'api': 'saveContentsSrc',
+				'page_path': px2ce.page_path,
+				'codes': codes,
+			},
+			function(result){
+				callback(result);
+
+				// it79.ary(
+				// 	droppedFileList,
+				// 	function(itAry1, row, idx){
+				// 		px2ce.gpiBridge(
+				// 			{
+				// 				'api': 'savePageResources',
+				// 				'page_path': px2ce.page_path,
+				// 				'filename': row.name,
+				// 				'base64': row.base64,
+				// 			},
+				// 			function(result){
+				// 				itAry1.next();
+				// 			}
+				// 		);
+				// 	},
+				// 	function(){
+				// 		droppedFileList = []; // アップロードしたら忘れて良い。
+				// 		callback(result);
+				// 	}
+				// );
+
+			}
+		);
+	}
 }
