e126e75a1605af75149f4a12a31990b7f18d8027
Tomoya Koyanagi <tomk79@gmail.com>
2025-05-29 01:08:58 +0900

    モジュール編集機能を追加した。(12)

--- php/moduleEditor_kflow.php
+++ php/moduleEditor_kflow.php
@@ -47,7 +47,61 @@ class moduleEditor_kflow{
 			'message' => 'OK'
 		);
 
-		// TODO: ビルド処理をここに実装する
+		$src_kflo = null;
+		if( is_file( $realpath_module_dir.'src/template.kflow' ) ){
+			$src_kflow = file_get_contents($realpath_module_dir.'src/template.kflow');
+		}
+		if( $src_kflow === null ){
+			$result['result'] = false;
+			$result['message'] = 'Template file not found.';
+			return $result;
+		}
+
+		$extraValues = array();
+
+		$kaleflower = new \kaleflower\kaleflower();
+		$kaleflower->loadXml( $src_kflow );
+		$kflowResult = $kaleflower->build(
+			array(
+				'assetsPrefix' => './template_files/resources/',
+				'extra' => $extraValues,
+			)
+		);
+
+		// --------------------------------------
+		// HTMLを保存
+		$template_html = $kflowResult->html->main ?? '';
+		if( !$this->is_authorized_server_side_scripting ){
+			$template_html = $this->sanitizer->sanitize($template_html);
+		}
+		$realpath_template_html = $realpath_module_dir.'template.html.twig';
+		$this->px2ce->fs()->save_file( $realpath_template_html, $template_html );
+
+		// --------------------------------------
+		// CSSを保存
+		$src_css = $kflowResult->css ?? '';
+		if( !$this->is_authorized_server_side_scripting ){
+			$src_css = $this->sanitizer->sanitize($src_css);
+		}
+		$realpath_scss = $realpath_module_dir.'module.css.scss';
+		if( !strlen($src_css ?? '') ){
+			$this->px2ce->fs()->rm( $realpath_scss );
+		}else{
+			$this->px2ce->fs()->save_file( $realpath_scss, $src_css );
+		}
+
+		// --------------------------------------
+		// JSを保存
+		$src_js = $kflowResult->js ?? '';
+		if( !$this->is_authorized_server_side_scripting ){
+			$src_js = $this->sanitizer->sanitize($src_js);
+		}
+		$realpath_js = $realpath_module_dir.'module.js';
+		if( !strlen($src_js ?? '') ){
+			$this->px2ce->fs()->rm( $realpath_js );
+		}else{
+			$this->px2ce->fs()->save_file( $realpath_js, $src_js );
+		}
 
 		return $result;
 	}
