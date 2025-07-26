1e94fd2cbbf85f77a317926961e3515e9625a623
Tomoya Koyanagi <tomk79@gmail.com>
2025-07-26 22:04:44 +0900

    module editor: kflow: 参照できる変数を見直した

--- src/moduleEditor/kflow/kflow.js
+++ src/moduleEditor/kflow/kflow.js
@@ -132,70 +132,30 @@ module.exports = function(px2ce){
 					const container = $canvas.find('.pickles2-contents-editor__module-editor-kflow').get(0);
 					const navigationInfo = px2ce.getBootupInfomations().navigationInfo;
 					const extraValues = {
-						'config': px2conf,
-						'topPageInfo': navigationInfo.top_page_info,
-						'currentPageInfo': navigationInfo.page_info,
-						'pageInfo': navigationInfo.page_info,
-						'breadcrumb': navigationInfo.breadcrumb_info,
-						'parent': navigationInfo.parent_info,
-						'bros': navigationInfo.bros_info,
-						'children': navigationInfo.children_info,
-						'globalMenu': navigationInfo.global_menu_info || null,
-						'shoulderMenu': navigationInfo.shoulder_menu_info || null,
-						'categoryTop': navigationInfo.category_top_info || null,
-						'categorySubMenu': navigationInfo.category_sub_menu_info || null,
-						'href': function(path){
-							return path.replace(/^\/*/, '/');
-						},
-						'getCurrentPageInfo': function(){
-							return navigationInfo.page_info;
-						},
-						'getPageInfo': function($path){
-							const newPage = {
-								...navigationInfo.page_info,
-								title: 'my page',
-								title_label: 'my page',
-								title_h1: 'my page',
-								title_breadcrumb: 'my page',
-								title_full: 'my page',
-							};
-							return newPage;
-						},
-						'getBros': function($path, $options){
-							return [
-								navigationInfo.page_info.id,
-								navigationInfo.page_info.id + 1,
-								navigationInfo.page_info.id + 2,
-								navigationInfo.page_info.id + 3,
-							];
-						},
-						'getChildren': function($path, $options){
-							return [
-								navigationInfo.page_info.id,
-								navigationInfo.page_info.id + 1,
-								navigationInfo.page_info.id + 2,
-								navigationInfo.page_info.id + 3,
-							];
-						},
-						'getCategoryTop': function($path){
-							return navigationInfo.category_top_info;
-						},
-						'getGlobalMenu': function(){
-							const globalMenu = [];
-							navigationInfo.global_menu_info.forEach((item) => {
-								globalMenu.push(item.id);
-							});
-							return globalMenu;
-						},
-						'getShoulderMenu': function(){
-							const shoulderMenu = [];
-							navigationInfo.shoulder_menu_info.forEach((item) => {
-								shoulderMenu.push(item.id);
-							});
-							return shoulderMenu;
-						},
-						'isPageInBreadcrumb': function($page_id){
-							return false;
+						"_ENV": {
+							"mode": "canvas",
+							"vars": {},
+							"lang": "en",
+							"data": {
+								"modId": "package:category/module",
+								"fields": {},
+								"anchor": "",
+								"dec": "",
+							},
+							"extra": {
+								'config': px2conf,
+								'topPageInfo': navigationInfo.top_page_info,
+								'currentPageInfo': navigationInfo.page_info,
+								'pageInfo': navigationInfo.page_info,
+								'breadcrumb': navigationInfo.breadcrumb_info,
+								'parent': navigationInfo.parent_info,
+								'bros': navigationInfo.bros_info,
+								'children': navigationInfo.children_info,
+								'globalMenu': navigationInfo.global_menu_info || null,
+								'shoulderMenu': navigationInfo.shoulder_menu_info || null,
+								'categoryTop': navigationInfo.category_top_info || null,
+								'categorySubMenu': navigationInfo.category_sub_menu_info || null,
+							},
 						},
 						'__PX2CE_MODULE_EDITOR_GET_INFOJSON__': function(){
 							return JSON.parse(codeInfoJson);
