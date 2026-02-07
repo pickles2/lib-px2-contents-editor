b00327756bf68ec0927c72c595002605c17c82ec
Tomoya Koyanagi <tomk79@gmail.com>
2026-02-08 00:44:08 +0900

    default editor: リファクタリング
    ``````txt
    次の内容でリファクタリングしてください。
    
    デフォルトのエディタ画面 `src/editor/default/default.js` で、画像をアップロードして、記事に挿入するダイアログがあります。
    
    これは `openInsertImageDialog()` に実装されています。
    
    この機能を、 `default.js` から別ファイルに分離してください。
    ``````

--- src/editor/default/default.js
+++ src/editor/default/default.js
@@ -9,6 +9,7 @@ module.exports = function(px2ce){
 	var $canvas = $(px2ce.getElmCanvas());
 	var page_path = px2ce.page_path;
 	var current_tab = 'html';
+	var insertImageDialogModule;
 	var _imgDummy = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYyMCIgaGVpZ2h0PSIxMDgwIiB2aWV3Qm94PSIwIDAgMTYyMCAxMDgwIiBm'+
 		'aWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgo8cmVjdCB3aWR0aD0iMTYyMCIgaGVpZ2h0PSIxMDgwIiBmaWxsPSIjOTk5OTk5IiBmaWxsLW9wYWNpdHk9IjAuMyIvPgo8ZyBjbGlwLXBhdGg9InVybCgjY2xpcDBfMjExMF80NDYzKSI+CjxwYXRoIGQ9Ik03NzIuMjg4IDQ5Mi44MTJDNzcyLjI4OCA1MDAuMzIxIDc2OS4zMDUgNTA3LjUyMyA3NjMuOTk1IDUxMi44MzJDNzU4LjY4NiA1MTguMTQyIDc1MS40ODQgNTIxLjEyNSA3NDMuOTc1IDUyMS4xMjVDNzM2LjQ2NiA1MjEuMTI1IDcyOS4yNjUgNTE4LjE0MiA3MjMuOTU1IDUxMi44MzJDNzE4LjY0NiA1MDcuNTIzIDcxNS42NjMgNTAwLjMyMSA3MTUuNjYzIDQ5Mi44MTJDNzE1LjY2MyA0ODUuMzA0IDcxOC42NDYgNDc4LjEwMiA3MjMuOTU1IDQ3Mi43OTNDNzI5LjI2NSA0NjcuNDgzIDczNi40Nj'+
 		'YgNDY0LjUgNzQzLjk3NSA0NjQuNUM3NTEuNDg0IDQ2NC41IDc1OC42ODYgNDY3LjQ4MyA3NjMuOTk1IDQ3Mi43OTNDNzY5LjMwNSA0NzguMTAyIDc3Mi4yODggNDg1LjMwNCA3NzIuMjg4IDQ5Mi44MTJaIiBmaWxsPSIjQUFBQUFBIiBmaWxsLW9wYWNpdHk9IjAuNyIvPgo8cGF0aCBkPSJNNjk2Ljc4OCA0MDcuODc1QzY4Ni43NzYgNDA3Ljg3NSA2NzcuMTc0IDQxMS44NTIgNjcwLjA5NCA0MTguOTMyQzY2My4wMTUgNDI2LjAxMSA2NTkuMDM4IDQzNS42MTMgNjU5LjAzOCA0NDUuNjI1VjYzNC4zNzVDNjU5LjAzOCA2NDQuMzg3IDY2My4wMTUgNjUzLjk4OSA2NzAuMDk0IDY2MS4wNjhDNjc3LjE3NCA2NjguMTQ4IDY4Ni43NzYgNjcyLjEyNSA2OTYuNzg4IDY3Mi4xMjVIOTIzLjI4OEM5MzMuMyA2NzIuMTI1IDk0Mi45MDIgNjY4LjE0OCA5NDkuOTgxIDY2MS4wNjhDOTU3LjA2MSA2'+
@@ -197,7 +198,7 @@ module.exports = function(px2ce){
 					toolbar.addButton({
 						"label": px2ce.lb.get('editor.default.insert_image'),
 						"click": function(){
-							openInsertImageDialog();
+							insertImageDialogModule.openInsertImageDialog();
 						}
 					});
 					toolbar.addButton({
@@ -279,10 +280,10 @@ module.exports = function(px2ce){
 						var droppedFileInfo = event.dataTransfer.files[0];
 
 						// mod.filename
-						readSelectedLocalFile(droppedFileInfo, function(_dataUri){
+						insertImageDialogModule.readSelectedLocalFile(droppedFileInfo, function(_dataUri){
 							var fileInfo = {
 								'name': droppedFileInfo.name,
-								'ext': getExtension( droppedFileInfo.name ),
+								'ext': insertImageDialogModule.getExtension( droppedFileInfo.name ),
 								'size': droppedFileInfo.size,
 								'type': droppedFileInfo.type,
 								"base64": _dataUri,
@@ -292,7 +293,7 @@ module.exports = function(px2ce){
 								"display": "none",
 							});
 
-							openInsertImageDialog( fileInfo );
+							insertImageDialogModule.openInsertImageDialog( fileInfo );
 						});
 					});
 
@@ -455,6 +456,19 @@ module.exports = function(px2ce){
 						'src': previewUrl,
 					})
 				;
+
+				// 画像挿入ダイアログモジュールを初期化
+				insertImageDialogModule = require('./insertImageDialog.js')({
+					px2ce: px2ce,
+					_imgDummy: _imgDummy,
+					droppedFileList: droppedFileList,
+					getCurrentTab: function(){ return current_tab; },
+					page_path: page_path,
+					px2conf: px2conf,
+					insertText: insertText,
+					saveContentsSrc: saveContentsSrc
+				});
+
 				it1.next(arg);
 			},
 		]);
@@ -494,373 +508,6 @@ module.exports = function(px2ce){
 
 		callback(true);
 		return;
-	}
-
-	/**
-	 * 画像挿入ダイアログを開く
-	 */
-	function openInsertImageDialog( presetInsertFileInfo ){
-
-		var $body = $(`<div>
-			<p>${px2ce.lb.get('editor.default.select_image_to_insert')}</p>
-			<div class="px2-form-input-list">
-				<ul class="px2-form-input-list__ul">
-					<li class="px2-form-input-list__li">
-						<div class="px2-form-input-list__label"><label for="insert-image-file">${px2ce.lb.get('editor.default.file')}</label></div>
-						<div class="px2-form-input-list__input">
-							<div class="pickles2-contents-editor__default-image-preview" tabindex="0">
-								<img class="pickles2-contents-editor__default-image-preview-image" />
-								<div class="pickles2-contents-editor__default-image-preview-no-image"></div>
-							</div>
-							<input type="file" id="insert-image-file" name="insert-image-file" value="" />
-						</div>
-					</li>
-					<li class="px2-form-input-list__li">
-						<div class="px2-form-input-list__label"><label for="insert-image-file-name">${px2ce.lb.get('editor.default.filename')}</label></div>
-						<div class="px2-form-input-list__input">
-							<input type="text" id="insert-image-file-name" name="insert-image-file-name" value="" class="px2-input px2-input--block" required />
-						</div>
-					</li>
-				</ul>
-			</div>
-			<input type="hidden" id="insert-image-original-file-name" name="insert-image-original-file-name" value="" />
-		</div>`);
-		var $imgPreview = $body.find('.pickles2-contents-editor__default-image-preview-image');
-		var $imgNotImage = $body.find('.pickles2-contents-editor__default-image-preview-no-image');
-
-		function isValidFilename(filename){
-			if( !filename.match(/^[a-z0-9\-\_]+\.[a-z0-9]+$/i) ){
-				return false;
-			}
-			return true;
-		}
-		function generateAutoFilename(filename){
-			var ext = filename.replace(/^[\s\S]*\./, '');
-			var today = new Date();
-			return dateformat(today, 'yyyy-mm-dd-hhMMss') + '.' + ext;
-		}
-
-		/**
-		 * 画像プレビューを更新する
-		 */
-		function setImagePreview(fileInfo){
-			var fileSrc = fileInfo.src;
-			var fileMimeType = fileInfo.mimeType;
-			if( !fileInfo || !fileInfo.src || !fileInfo.ext || !fileInfo.size){
-				fileSrc = px2ce.getNoimagePlaceholder() || _imgDummy;
-				fileMimeType = 'image/svg+xml';
-			}
-			$imgPreview
-				.attr({
-					"src": fileSrc,
-				})
-			;
-			$imgNotImage.text( fileInfo.ext );
-			if( canFilePreviewAsImage(fileMimeType, fileInfo.ext) ){
-				$imgPreview.show();
-				$imgNotImage.hide();
-			}else{
-				$imgPreview.hide();
-				$imgNotImage.show();
-			}
-			return;
-		}
-
-		var modalObj = px2style.modal({
-			"title": px2ce.lb.get('editor.default.insert_image_dialog_title'),
-			"body": $body,
-			"form": {
-				"submit": function(){
-					var $inputFile = $body.find('input[name=insert-image-file]');
-					var $inputFileName = $body.find('input[name=insert-image-file-name]');
-					var $inputOriginalFileName = $body.find('input[name=insert-image-original-file-name]');
-					var fileInfoJSON = $inputFile.attr('data-upload-file');
-					if( !fileInfoJSON ){
-						return;
-					}
-					if( !$inputFileName.val() ){
-						return;
-					}
-
-					if( !isValidFilename($inputFileName.val()) ){
-						alert(px2ce.lb.get('editor.default.invalid_filename'));
-						return;
-					}
-
-					var fileInfo = JSON.parse(fileInfoJSON);
-					fileInfo.name = (function(){
-						if( !isValidFilename($inputFileName.val()) ){
-							return generateAutoFilename($inputFileName.val());
-						}
-						return $inputFileName.val();
-					})();
-
-					insertUploadFile(fileInfo, $inputOriginalFileName.val());
-					modalObj.close();
-				}
-			},
-			"buttons": [
-				$(`<button type="submit" class="px2-btn px2-btn--primary">${px2ce.lb.get('editor.default.insert_button')}</button>`),
-			],
-		}, function(){
-			var $inputFile = $body.find('input[name=insert-image-file]');
-			var $inputFileName = $body.find('input[name=insert-image-file-name]');
-			var $inputOriginalFileName = $body.find('input[name=insert-image-original-file-name]');
-
-			setImagePreview({});
-
-			if( typeof(presetInsertFileInfo) == typeof({}) ){
-				setImagePreview({
-					'src': presetInsertFileInfo.base64,
-					'ext': getExtension(presetInsertFileInfo.name),
-					'size': presetInsertFileInfo.size,
-					'mimeType': presetInsertFileInfo.type,
-				});
-				$inputFile.attr({
-					'data-upload-file': JSON.stringify({
-						'name': presetInsertFileInfo.name,
-						'ext': getExtension(presetInsertFileInfo.name),
-						'size': presetInsertFileInfo.size,
-						'type': presetInsertFileInfo.type,
-						'base64': presetInsertFileInfo.base64,
-					})
-				});
-				$inputFileName.val((function(){
-					if( !isValidFilename(presetInsertFileInfo.name) ){
-						return generateAutoFilename(presetInsertFileInfo.name);
-					}
-					return presetInsertFileInfo.name;
-				})());
-			}
-
-			$inputFile
-				.on('change', function(e){
-					var $this = $(this);
-					var fileInfo = e.target.files[0];
-					var realpathSelected = $this.val();
-
-					$inputOriginalFileName.val(fileInfo.name);
-
-					if( realpathSelected ){
-						readSelectedLocalFile(fileInfo, function(dataUri){
-							setImagePreview({
-								'src': dataUri,
-								'ext': getExtension(fileInfo.name),
-								'size': fileInfo.size,
-								'mimeType': fileInfo.type,
-							});
-							$this.attr({
-								'data-upload-file': JSON.stringify({
-									'name': fileInfo.name,
-									'ext': getExtension(fileInfo.name),
-									'size': fileInfo.size,
-									'type': fileInfo.type,
-									'base64': dataUri,
-								})
-							});
-							$inputFileName.val((function(){
-								if( !isValidFilename(fileInfo.name) ){
-									return generateAutoFilename(fileInfo.name);
-								}
-								return fileInfo.name;
-							})());
-						});
-					}
-				});
-
-			var $imagePreviewArea = $body.find('.pickles2-contents-editor__default-image-preview');
-			$imagePreviewArea
-				.on('paste', function(e){
-					var items = e.originalEvent.clipboardData.items;
-					for (var i = 0 ; i < items.length ; i++) {
-						var item = items[i];
-						var fileInfo = item.getAsFile();
-
-						$inputOriginalFileName.val(fileInfo.name);
-
-						fileInfo.name = fileInfo.name||'clipboard.'+(function(type){
-							if(type.match(/png$/i)){return 'png';}
-							if(type.match(/gif$/i)){return 'gif';}
-							if(type.match(/(?:jpeg|jpg|jpe)$/i)){return 'jpg';}
-							if(type.match(/webp$/i)){return 'webp';}
-							if(type.match(/svg/i)){return 'svg';}
-							return getExtension(fileInfo.name);
-						})(fileInfo.type);
-
-						// mod.filename
-						readSelectedLocalFile(fileInfo, function(dataUri){
-							setImagePreview({
-								'src': dataUri,
-								'ext': getExtension(fileInfo.name),
-								'size': fileInfo.size,
-								'mimeType': fileInfo.type,
-							});
-							$inputFile.attr({
-								'data-upload-file': JSON.stringify({
-									'name': fileInfo.name,
-									'ext': getExtension(fileInfo.name),
-									'size': fileInfo.size,
-									'type': fileInfo.type,
-									'base64': dataUri,
-								})
-							});
-							$inputFileName.val( generateAutoFilename(fileInfo.name) );
-						});
-					}
-				})
-				.on('dragover', function(e){
-					e.stopPropagation();
-					e.preventDefault();
-					$(this).css({
-						'outline': '3px dashed #0066cc',
-						'outline-offset': '-3px',
-					});
-				})
-				.on('dragleave', function(e){
-					e.stopPropagation();
-					e.preventDefault();
-					$(this).css({
-						'outline': '',
-						'outline-offset': '',
-					});
-				})
-				.on('drop', function(e){
-					e.stopPropagation();
-					e.preventDefault();
-					$(this).css({
-						'outline': '',
-						'outline-offset': '',
-					});
-
-					var event = e.originalEvent;
-					var droppedFileInfo = event.dataTransfer.files[0];
-
-					$inputOriginalFileName.val(droppedFileInfo.name);
-
-					// mod.filename
-					readSelectedLocalFile(droppedFileInfo, function(_dataUri){
-						var fileInfo = {
-							'name': droppedFileInfo.name,
-							'ext': getExtension( droppedFileInfo.name ),
-							'size': droppedFileInfo.size,
-							'type': droppedFileInfo.type,
-						};
-
-						setImagePreview({
-							'src': _dataUri,
-							'ext': fileInfo.ext,
-							'size': fileInfo.size,
-							'mimeType': fileInfo.type,
-						});
-						$inputFile.attr({
-							'data-upload-file': JSON.stringify({
-								'name': fileInfo.name,
-								'ext': fileInfo.ext,
-								'size': fileInfo.size,
-								'type': fileInfo.type,
-								'base64': _dataUri,
-							})
-						});
-						$inputFileName.val((function(){
-							if( !isValidFilename(fileInfo.name) ){
-								return generateAutoFilename(fileInfo.name);
-							}
-							return fileInfo.name;
-						})());
-					});
-				});
-
-		});
-	}
-
-	/**
-	 * アップロードしたファイルをコンテンツに挿入する
-	 */
-	function insertUploadFile(fileInfo, originalFileName, callback){
-		var path_resource;
-
-		it79.fnc({}, [
-			function(it1){
-				px2ce.gpiBridge(
-					{
-						'api': 'getPathResources',
-						'page_path': page_path
-					},
-					function(result){
-						var path = require('path');
-						var tmpPathControot = px2conf.path_controot;
-						tmpPathControot = tmpPathControot.replace(/\/+$/, '')+page_path;
-						tmpPathControot = tmpPathControot.replace(/[^\/]*$/, '');
-						var relative_path = path.relative(tmpPathControot, result);
-						path_resource = relative_path;
-						it1.next();
-					}
-				);
-			},
-			function(it1){
-				var fileName = fileInfo.name;
-				// var uploadFileName = './'+path_resource+'/'+fileName;
-				var uploadFileName = '<'; // NOTE: minifyされたあと、PHPコードとして成立してしまわないように、複数行に分解している。
-				uploadFileName += '?';
-				uploadFileName += `= $px->h($px->path_files("/${fileName}")) ?`;
-				uploadFileName += '>';
-				var insertString = '';
-
-
-				// 開いているタブの種類に応じて、
-				// 挿入する文字列を出し分ける。
-				switch(current_tab){
-					case 'css':
-						insertString = `url("${uploadFileName}")`;
-						break;
-					case 'js':
-						insertString = `"${uploadFileName}"`;
-						break;
-					case 'html':
-					default:
-						if( fileInfo.type.match(/^image\//) ){
-							insertString = `<img src="${uploadFileName}" alt="${originalFileName || fileName}" />`+"\n";
-						}else{
-							insertString = `<a href="${uploadFileName}" download="${fileName}">${originalFileName || fileName}</a>`+"\n";
-						}
-						break;
-				}
-
-				// アップロードファイルを一時記憶
-				// ファイルは、次回保存時に保存されます。
-				droppedFileList.push({
-					'name': fileInfo.name,
-					'type': fileInfo.type,
-					'size': fileInfo.size,
-					'base64': fileInfo.base64,
-				});
-
-				// コンテンツに文字列を挿入する
-				insertText( insertString, current_tab );
-
-				it1.next();
-			},
-			function(it1){
-				saveContentsSrc(function(){
-					it1.next();
-				});
-			},
-			function(){
-				callback();
-			}
-		]);
-		return;
-	}
-
-	/**
-	 * アップロードファイルを読み込む
-	 */
-	function readSelectedLocalFile(fileInfo, callback){
-		var reader = new FileReader();
-		reader.onload = function(evt) {
-			callback( evt.target.result );
-		}
-		reader.readAsDataURL(fileInfo);
 	}
 
 	/**
@@ -975,43 +622,6 @@ module.exports = function(px2ce){
 			},
 		]);
 		return;
-	}
-
-	/**
-	 * パスから拡張子を取り出して返す
-	 */
-	function getExtension(path){
-		var ext = '';
-		try {
-			var ext = path.replace( new RegExp('^.*?\.([a-zA-Z0-9\_\-]+)$'), '$1' );
-			ext = ext.toLowerCase();
-		} catch (e) {
-			ext = false;
-		}
-		return ext;
-	}
-
-	/**
-	 * 画像としてプレビューできる種類か評価する
-	 */
-	function canFilePreviewAsImage(mimetype, ext){
-		if( mimetype ){
-			if( mimetype.match(/^image\//) ){
-				return true;
-			}
-		}else if( ext ){
-			switch( ext ){
-				case 'jpg':
-				case 'jpeg':
-				case 'jpe':
-				case 'png':
-				case 'gif':
-				case 'webp':
-					return true;
-					break;
-			}
-		}
-		return false;
 	}
 
 	/**
---
+++ src/editor/default/insertImageDialog.js
@@ -0,0 +1,430 @@
+/**
+ * insertImageDialog.js
+ * 画像挿入ダイアログの機能を提供
+ */
+module.exports = function(options){
+	var px2ce = options.px2ce;
+	var _imgDummy = options._imgDummy;
+	var droppedFileList = options.droppedFileList;
+	var getCurrentTab = options.getCurrentTab;
+	var page_path = options.page_path;
+	var px2conf = options.px2conf;
+	var insertText = options.insertText;
+	var saveContentsSrc = options.saveContentsSrc;
+
+	var $ = require('jquery');
+	var dateformat = require('dateformat');
+	var it79 = require('iterate79');
+
+	/**
+	 * パスから拡張子を取り出して返す
+	 */
+	function getExtension(path){
+		var ext = '';
+		try {
+			var ext = path.replace( new RegExp('^.*?\.([a-zA-Z0-9\_\-]+)$'), '$1' );
+			ext = ext.toLowerCase();
+		} catch (e) {
+			ext = false;
+		}
+		return ext;
+	}
+
+	/**
+	 * 画像としてプレビューできる種類か評価する
+	 */
+	function canFilePreviewAsImage(mimetype, ext){
+		if( mimetype ){
+			if( mimetype.match(/^image\//) ){
+				return true;
+			}
+		}else if( ext ){
+			switch( ext ){
+				case 'jpg':
+				case 'jpeg':
+				case 'jpe':
+				case 'png':
+				case 'gif':
+				case 'webp':
+					return true;
+					break;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * アップロードファイルを読み込む
+	 */
+	function readSelectedLocalFile(fileInfo, callback){
+		var reader = new FileReader();
+		reader.onload = function(evt) {
+			callback( evt.target.result );
+		}
+		reader.readAsDataURL(fileInfo);
+	}
+
+	/**
+	 * アップロードしたファイルをコンテンツに挿入する
+	 */
+	function insertUploadFile(fileInfo, originalFileName, callback){
+		callback = callback || function(){};
+		var path_resource;
+
+		it79.fnc({}, [
+			function(it1){
+				px2ce.gpiBridge(
+					{
+						'api': 'getPathResources',
+						'page_path': page_path
+					},
+					function(result){
+						var path = require('path');
+						var tmpPathControot = px2conf.path_controot;
+						tmpPathControot = tmpPathControot.replace(/\/+$/, '')+page_path;
+						tmpPathControot = tmpPathControot.replace(/[^\/]*$/, '');
+						var relative_path = path.relative(tmpPathControot, result);
+						path_resource = relative_path;
+						it1.next();
+					}
+				);
+			},
+			function(it1){
+				var fileName = fileInfo.name;
+				// var uploadFileName = './'+path_resource+'/'+fileName;
+				var uploadFileName = '<'; // NOTE: minifyされたあと、PHPコードとして成立してしまわないように、複数行に分解している。
+				uploadFileName += '?';
+				uploadFileName += `= $px->h($px->path_files("/${fileName}")) ?`;
+				uploadFileName += '>';
+				var insertString = '';
+
+				var current_tab = getCurrentTab();
+
+				// 開いているタブの種類に応じて、
+				// 挿入する文字列を出し分ける。
+				switch(current_tab){
+					case 'css':
+						insertString = `url("${uploadFileName}")`;
+						break;
+					case 'js':
+						insertString = `"${uploadFileName}"`;
+						break;
+					case 'html':
+					default:
+						if( fileInfo.type.match(/^image\//) ){
+							insertString = `<img src="${uploadFileName}" alt="${originalFileName || fileName}" />`+"\n";
+						}else{
+							insertString = `<a href="${uploadFileName}" download="${fileName}">${originalFileName || fileName}</a>`+"\n";
+						}
+						break;
+				}
+
+				// アップロードファイルを一時記憶
+				// ファイルは、次回保存時に保存されます。
+				droppedFileList.push({
+					'name': fileInfo.name,
+					'type': fileInfo.type,
+					'size': fileInfo.size,
+					'base64': fileInfo.base64,
+				});
+
+				// コンテンツに文字列を挿入する
+				insertText( insertString, current_tab );
+
+				it1.next();
+			},
+			function(it1){
+				saveContentsSrc(function(){
+					it1.next();
+				});
+			},
+			function(){
+				callback();
+			}
+		]);
+		return;
+	}
+
+	/**
+	 * 画像挿入ダイアログを開く
+	 */
+	function openInsertImageDialog( presetInsertFileInfo ){
+
+		var $body = $(`<div>
+			<p>${px2ce.lb.get('editor.default.select_image_to_insert')}</p>
+			<div class="px2-form-input-list">
+				<ul class="px2-form-input-list__ul">
+					<li class="px2-form-input-list__li">
+						<div class="px2-form-input-list__label"><label for="insert-image-file">${px2ce.lb.get('editor.default.file')}</label></div>
+						<div class="px2-form-input-list__input">
+							<div class="pickles2-contents-editor__default-image-preview" tabindex="0">
+								<img class="pickles2-contents-editor__default-image-preview-image" />
+								<div class="pickles2-contents-editor__default-image-preview-no-image"></div>
+							</div>
+							<input type="file" id="insert-image-file" name="insert-image-file" value="" />
+						</div>
+					</li>
+					<li class="px2-form-input-list__li">
+						<div class="px2-form-input-list__label"><label for="insert-image-file-name">${px2ce.lb.get('editor.default.filename')}</label></div>
+						<div class="px2-form-input-list__input">
+							<input type="text" id="insert-image-file-name" name="insert-image-file-name" value="" class="px2-input px2-input--block" required />
+						</div>
+					</li>
+				</ul>
+			</div>
+			<input type="hidden" id="insert-image-original-file-name" name="insert-image-original-file-name" value="" />
+		</div>`);
+		var $imgPreview = $body.find('.pickles2-contents-editor__default-image-preview-image');
+		var $imgNotImage = $body.find('.pickles2-contents-editor__default-image-preview-no-image');
+
+		function isValidFilename(filename){
+			if( !filename.match(/^[a-z0-9\-\_]+\.[a-z0-9]+$/i) ){
+				return false;
+			}
+			return true;
+		}
+		function generateAutoFilename(filename){
+			var ext = filename.replace(/^[\s\S]*\./, '');
+			var today = new Date();
+			return dateformat(today, 'yyyy-mm-dd-hhMMss') + '.' + ext;
+		}
+
+		/**
+		 * 画像プレビューを更新する
+		 */
+		function setImagePreview(fileInfo){
+			var fileSrc = fileInfo.src;
+			var fileMimeType = fileInfo.mimeType;
+			if( !fileInfo || !fileInfo.src || !fileInfo.ext || !fileInfo.size){
+				fileSrc = px2ce.getNoimagePlaceholder() || _imgDummy;
+				fileMimeType = 'image/svg+xml';
+			}
+			$imgPreview
+				.attr({
+					"src": fileSrc,
+				})
+			;
+			$imgNotImage.text( fileInfo.ext );
+			if( canFilePreviewAsImage(fileMimeType, fileInfo.ext) ){
+				$imgPreview.show();
+				$imgNotImage.hide();
+			}else{
+				$imgPreview.hide();
+				$imgNotImage.show();
+			}
+			return;
+		}
+
+		var modalObj = px2style.modal({
+			"title": px2ce.lb.get('editor.default.insert_image_dialog_title'),
+			"body": $body,
+			"form": {
+				"submit": function(){
+					var $inputFile = $body.find('input[name=insert-image-file]');
+					var $inputFileName = $body.find('input[name=insert-image-file-name]');
+					var $inputOriginalFileName = $body.find('input[name=insert-image-original-file-name]');
+					var fileInfoJSON = $inputFile.attr('data-upload-file');
+					if( !fileInfoJSON ){
+						return;
+					}
+					if( !$inputFileName.val() ){
+						return;
+					}
+
+					if( !isValidFilename($inputFileName.val()) ){
+						alert(px2ce.lb.get('editor.default.invalid_filename'));
+						return;
+					}
+
+					var fileInfo = JSON.parse(fileInfoJSON);
+					fileInfo.name = (function(){
+						if( !isValidFilename($inputFileName.val()) ){
+							return generateAutoFilename($inputFileName.val());
+						}
+						return $inputFileName.val();
+					})();
+
+					insertUploadFile(fileInfo, $inputOriginalFileName.val());
+					modalObj.close();
+				}
+			},
+			"buttons": [
+				$(`<button type="submit" class="px2-btn px2-btn--primary">${px2ce.lb.get('editor.default.insert_button')}</button>`),
+			],
+		}, function(){
+			var $inputFile = $body.find('input[name=insert-image-file]');
+			var $inputFileName = $body.find('input[name=insert-image-file-name]');
+			var $inputOriginalFileName = $body.find('input[name=insert-image-original-file-name]');
+
+			setImagePreview({});
+
+			if( typeof(presetInsertFileInfo) == typeof({}) ){
+				setImagePreview({
+					'src': presetInsertFileInfo.base64,
+					'ext': getExtension(presetInsertFileInfo.name),
+					'size': presetInsertFileInfo.size,
+					'mimeType': presetInsertFileInfo.type,
+				});
+				$inputFile.attr({
+					'data-upload-file': JSON.stringify({
+						'name': presetInsertFileInfo.name,
+						'ext': getExtension(presetInsertFileInfo.name),
+						'size': presetInsertFileInfo.size,
+						'type': presetInsertFileInfo.type,
+						'base64': presetInsertFileInfo.base64,
+					})
+				});
+				$inputFileName.val((function(){
+					if( !isValidFilename(presetInsertFileInfo.name) ){
+						return generateAutoFilename(presetInsertFileInfo.name);
+					}
+					return presetInsertFileInfo.name;
+				})());
+			}
+
+			$inputFile
+				.on('change', function(e){
+					var $this = $(this);
+					var fileInfo = e.target.files[0];
+					var realpathSelected = $this.val();
+
+					$inputOriginalFileName.val(fileInfo.name);
+
+					if( realpathSelected ){
+						readSelectedLocalFile(fileInfo, function(dataUri){
+							setImagePreview({
+								'src': dataUri,
+								'ext': getExtension(fileInfo.name),
+								'size': fileInfo.size,
+								'mimeType': fileInfo.type,
+							});
+							$this.attr({
+								'data-upload-file': JSON.stringify({
+									'name': fileInfo.name,
+									'ext': getExtension(fileInfo.name),
+									'size': fileInfo.size,
+									'type': fileInfo.type,
+									'base64': dataUri,
+								})
+							});
+							$inputFileName.val((function(){
+								if( !isValidFilename(fileInfo.name) ){
+									return generateAutoFilename(fileInfo.name);
+								}
+								return fileInfo.name;
+							})());
+						});
+					}
+				});
+
+			var $imagePreviewArea = $body.find('.pickles2-contents-editor__default-image-preview');
+			$imagePreviewArea
+				.on('paste', function(e){
+					var items = e.originalEvent.clipboardData.items;
+					for (var i = 0 ; i < items.length ; i++) {
+						var item = items[i];
+						var fileInfo = item.getAsFile();
+
+						$inputOriginalFileName.val(fileInfo.name);
+
+						fileInfo.name = fileInfo.name||'clipboard.'+(function(type){
+							if(type.match(/png$/i)){return 'png';}
+							if(type.match(/gif$/i)){return 'gif';}
+							if(type.match(/(?:jpeg|jpg|jpe)$/i)){return 'jpg';}
+							if(type.match(/webp$/i)){return 'webp';}
+							if(type.match(/svg/i)){return 'svg';}
+							return getExtension(fileInfo.name);
+						})(fileInfo.type);
+
+						// mod.filename
+						readSelectedLocalFile(fileInfo, function(dataUri){
+							setImagePreview({
+								'src': dataUri,
+								'ext': getExtension(fileInfo.name),
+								'size': fileInfo.size,
+								'mimeType': fileInfo.type,
+							});
+							$inputFile.attr({
+								'data-upload-file': JSON.stringify({
+									'name': fileInfo.name,
+									'ext': getExtension(fileInfo.name),
+									'size': fileInfo.size,
+									'type': fileInfo.type,
+									'base64': dataUri,
+								})
+							});
+							$inputFileName.val( generateAutoFilename(fileInfo.name) );
+						});
+					}
+				})
+				.on('dragover', function(e){
+					e.stopPropagation();
+					e.preventDefault();
+					$(this).css({
+						'outline': '3px dashed #0066cc',
+						'outline-offset': '-3px',
+					});
+				})
+				.on('dragleave', function(e){
+					e.stopPropagation();
+					e.preventDefault();
+					$(this).css({
+						'outline': '',
+						'outline-offset': '',
+					});
+				})
+				.on('drop', function(e){
+					e.stopPropagation();
+					e.preventDefault();
+					$(this).css({
+						'outline': '',
+						'outline-offset': '',
+					});
+
+					var event = e.originalEvent;
+					var droppedFileInfo = event.dataTransfer.files[0];
+
+					$inputOriginalFileName.val(droppedFileInfo.name);
+
+					// mod.filename
+					readSelectedLocalFile(droppedFileInfo, function(_dataUri){
+						var fileInfo = {
+							'name': droppedFileInfo.name,
+							'ext': getExtension( droppedFileInfo.name ),
+							'size': droppedFileInfo.size,
+							'type': droppedFileInfo.type,
+						};
+
+						setImagePreview({
+							'src': _dataUri,
+							'ext': fileInfo.ext,
+							'size': fileInfo.size,
+							'mimeType': fileInfo.type,
+						});
+						$inputFile.attr({
+							'data-upload-file': JSON.stringify({
+								'name': fileInfo.name,
+								'ext': fileInfo.ext,
+								'size': fileInfo.size,
+								'type': fileInfo.type,
+								'base64': _dataUri,
+							})
+						});
+						$inputFileName.val((function(){
+							if( !isValidFilename(fileInfo.name) ){
+								return generateAutoFilename(fileInfo.name);
+							}
+							return fileInfo.name;
+						})());
+					});
+				});
+
+		});
+	}
+
+	return {
+		openInsertImageDialog: openInsertImageDialog,
+		getExtension: getExtension,
+		readSelectedLocalFile: readSelectedLocalFile
+	};
+};
